84a53d75883538bcbb1d0be15aca4d65de7baf30
Francisco Renteria <renteriav@hotmail.com>
2016-12-05 16:24:59 -0700

    initial commit

---
+++ lib/finicity/v1/response/login_form.rb
@@ -0,0 +1,33 @@
+module Finicity::V1
+  module Response
+    class LoginField
+      include ::Saxomattic
+
+      ##
+      # Saxomattic Attributes
+      #
+      attribute :description
+      attribute :displayOrder, :type => ::Integer, :as => :display_order
+      attribute :id
+      attribute :name
+      attribute :mask
+      attribute :value
+      attribute :instructions
+      attribute :valueLengthMin, :type => ::Integer, :as => :value_length_min
+      attribute :valueLengthMax, :type => ::Integer, :as => :value_length_max
+
+      def mask?
+        mask == 'true'
+      end
+    end
+
+    class LoginForm
+      include ::Saxomattic
+
+      ##
+      # Saxomattic Attributes
+      #
+      attribute :loginField, :elements => true, :class => ::Finicity::V1::Response::LoginField, :as => :login_fields
+    end
+  end
+end
